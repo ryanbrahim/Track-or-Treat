3cec3ba379c7c8a101925a020384d66be4c7aef2
Ryan Almizyed <almizyed@wisc.edu>
2022-10-15 23:17:54 -0500

    Debugging prints

--- facerec-test/facerec-test.py
+++ facerec-test/facerec-test.py
@@ -3,6 +3,7 @@
 import face_recognition
 import cv2
 import numpy as np
+import faulthandler
 
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
@@ -17,12 +18,18 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
+print("Looking for Obama face")
+obama_image = face_recognition.load_image_file("C:\\Users\\ryanb\\Desktop\\repos\\Track-or-Treat\\facerec-test\\obama.jpg")
+print("Found Obama!")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+print("Encoded Obama!")
 
 # Load a second sample picture and learn how to recognize it.
-ryan_image = face_recognition.load_image_file("ryan.jpg")
+print("Looking for Ryan face")
+ryan_image = face_recognition.load_image_file("C:\\Users\\ryanb\\Desktop\\repos\\Track-or-Treat\\facerec-test\\ryan.jpg")
+print("Found Ryan!")
 ryan_face_encoding = face_recognition.face_encodings(ryan_image)[0]
+print("Encoded Ryan!")
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
@@ -42,19 +49,27 @@ process_this_frame = True
 
 while True:
     # Grab a single frame of video
+    print("Grabbing a single frame!")
     ret, frame = video_capture.read()
 
     # Only process every other frame of video to save time
     if process_this_frame:
+        print("We are processing this frame")
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        print("Small frame made")
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
+        print("Conversion done")
         
         # Find all the faces and face encodings in the current frame of video
+        # print(rgb_small_frame)
+        faulthandler.enable()
         face_locations = face_recognition.face_locations(rgb_small_frame)
+        print("Found the face locations!")
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        print("Processed frame and found face locations")
 
         face_names = []
         for face_encoding in face_encodings:
@@ -95,6 +110,7 @@ while True:
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
+    print("Displaying resulting image!")
     cv2.imshow('Video', frame)
 
     # Hit 'q' on the keyboard to quit!
